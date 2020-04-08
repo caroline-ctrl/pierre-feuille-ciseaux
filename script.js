6cadd9824cb1fc9177a6b97cd24162f41fcca15c
caroline-ctrl <duon.caroline@gmail.com>
2020-04-08 17:40:28 +0200

    amélioration

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-let scores = {pointPerso:0, pointOrdi:0}
+let scores = {
+    pointPerso: 0,
+    pointOrdi: 0
+}
 
 ////////////////////////////////////////////////
 // permet de cacher le choix du nombre de manche
@@ -56,18 +59,14 @@ function choicePerso(value) {
 
 //////////////////////////////////////////////////////
 // Arrete le jeu quand le nombre de points est atteint
-function breakParty()
-{
+function breakParty() {
     let nbr = document.getElementById("choiceParty").textContent
 
-    if (nbr == scores.pointOrdi){
+    if (nbr == scores.pointOrdi) {
         location.replace("/finishOrdi.html")
     } else if (nbr == scores.pointPerso) {
         location.replace("/finishPerso.html")
     }
-    console.log(nbr)
-    console.log(scores.pointOrdi)
-    console.log(scores.pointPerso)
 }
 
 
@@ -86,42 +85,42 @@ function rules() {
         scores.pointOrdi++
         document.getElementById("scorePerso").innerHTML = scores.pointPerso
         document.getElementById("scoreOrdi").innerHTML = scores.pointOrdi
-            return scores.pointOrdi
+        return scores.pointOrdi
 
     } else if ((perso == "PIERRE") && (ordi == "CISEAUX")) {
         document.getElementById("result").innerHTML = "Vous avez 1 point : La pierre broie les ciseaux"
         scores.pointPerso++
         document.getElementById("scorePerso").innerHTML = scores.pointPerso
         document.getElementById("scoreOrdi").innerHTML = scores.pointOrdi
-            return scores.pointPerso
+        return scores.pointPerso
 
     } else if ((perso == "FEUILLE") && (ordi == "PIERRE")) {
         document.getElementById("result").innerHTML = "Vous avez 1 point : La feuille enveloppe la pierre"
         scores.pointPerso++
         document.getElementById("scorePerso").innerHTML = scores.pointPerso
         document.getElementById("scoreOrdi").innerHTML = scores.pointOrdi
-            return scores.pointPerso
+        return scores.pointPerso
 
     } else if ((perso == "FEUILLE") && (ordi == "CISEAUX")) {
         document.getElementById("result").innerHTML = "Vous perdez le point : Les ciseaux coupent la feuille"
         scores.pointOrdi++
         document.getElementById("scorePerso").innerHTML = scores.pointPerso
         document.getElementById("scoreOrdi").innerHTML = scores.pointOrdi
-            return scores.pointOrdi
+        return scores.pointOrdi
 
     } else if ((perso == "CISEAUX") && (ordi == "PIERRE")) {
         document.getElementById("result").innerHTML = "Vous perdez le point : La pierre broie les ciseaux"
         scores.pointOrdi++
         document.getElementById("scorePerso").innerHTML = scores.pointPerso
         document.getElementById("scoreOrdi").innerHTML = scores.pointOrdi
-            return scores.pointOrdi
+        return scores.pointOrdi
 
     } else if ((perso == "CISEAUX") && (ordi == "FEUILLE")) {
         document.getElementById("result").innerHTML = "Vous avez 1 point : Les ciseaux coupent la feuille"
         scores.pointPerso++
         document.getElementById("scorePerso").innerHTML = scores.pointPerso
         document.getElementById("scoreOrdi").innerHTML = scores.pointOrdi
-            return scores.pointPerso
+        return scores.pointPerso
 
     }
 }
